a7945e933c88e8603435ddea3bd96b41cac38356
artin <msm2024@gmail.com>
2018-07-31 15:50:49 -0700

    f

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -9,7 +9,7 @@ Usage:
 from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
-
+import numpy as np
 import os
 import io
 import pandas as pd
